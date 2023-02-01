f425aa90b7c83777f0d8b9d05458dc367c304a45
Antonio Paulino <tonnyarauujo@gmail.com>
2023-02-01 10:44:28 -0300

    add player object to act like api data

--- src/components/RpgSheet.js
+++ src/components/RpgSheet.js
@@ -1,22 +1,25 @@
-import { Attributes } from "./Attributes"
+import { player } from "../entries/players"
+import { AttributesList } from "./AttributesList"
 import { Character } from "./Character"
 import { HpMpStats } from "./HpMpStats"
 import { LvlExpStats } from "./LvlExpStats"
 
 export const RpgSheet = () => {
+  const { name, surname, hp, mp, level, experience, attributes } = player
+
   return (
-    <div class="container">
+    <div className="container">
       <header>
-        <div class="header--primary">
-          <Character name={"Tovrax"} surName={"Brave"} />
+        <div className="header--primary">
+          <Character name={name} surname={surname} />
         </div>
-        <div class="header--secondary">
-          <HpMpStats hp={10} mp={8} />
-          <LvlExpStats lvl={1} xp={8} />
+        <div className="header--secondary">
+          <HpMpStats hp={hp} mp={mp} />
+          <LvlExpStats lvl={level} xp={experience} />
         </div>
       </header>
       <section>
-        <Attributes strength={10} wisdom={10} intelligency={10} agility={10} charisma={10} dexterity={10} />
+        <AttributesList attributes={attributes} />
       </section>
     </div>
   )
---
+++ src/entries/players.js
@@ -0,0 +1,16 @@
+export const player = {
+  name: "Tovlax",
+  surname: "Brave",
+  hp: 10,
+  mp: 10,
+  level: 1,
+  experience: 1,
+  attributes: {
+    strength: 10,
+    wisdom: 10,
+    intelligency: 10,
+    agility: 10,
+    charisma: 10,
+    dexterity: 10
+  }
+}
